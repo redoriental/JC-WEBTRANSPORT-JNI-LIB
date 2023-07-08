a6ae4ea9700a91becc939d3c924ca8593d7b37bc
redoriental <94785840+redoriental@users.noreply.github.com>
2023-07-09 00:01:22 +0800

    Update JCMain.java

--- test/JCMain.java
+++ test/JCMain.java
@@ -19,36 +19,6 @@ public class JCMain {
                 .setSsl("E:\\JCTEST\\ssl\\redoriental.top.key","E:\\JCTEST\\ssl\\redoriental.top_bundle.crt")
                 .run();
 
-//        try {
-//            // 加载原始图片
-//            BufferedImage originalImage = ImageIO.read(new File("E:\\JCTEST\\ssl\\img_1.png"));
-//
-//            // 创建一个新的空白图片，与原始图片大小相同
-//            BufferedImage newImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-//
-//            // 处理每个像素
-//            for (int y = 0; y < originalImage.getHeight(); y++) {
-//                for (int x = 0; x < originalImage.getWidth(); x++) {
-//                    // 获取当前像素的颜色
-//                    Color color = new Color(originalImage.getRGB(x, y));
-//
-//                    // 检查是否为黑色（RGB值全为0）
-//                    if (color.getRed() >= 110 && color.getGreen() >= 110 && color.getBlue() >= 110) {
-//                        // 将黑色像素设置为白色（RGB值全为255）
-//                        color = Color.WHITE;
-//                    }
-//
-//                    // 在新图片上设置当前像素的颜色
-//                    newImage.setRGB(x, y, color.getRGB());
-//                }
-//            }
-//
-//            // 保存处理后的图片
-//            ImageIO.write(newImage, "jpg", new File("E:\\JCTEST\\ssl\\img2.png"));
-//
-//            System.out.println("Image processing complete.");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
     }
 }
